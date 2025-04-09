8189267da34c9d289693875b9934a3160a2b1167
unabletoIoad <banana1234hah@gmail.com>
2025-04-09 19:05:12 +0200

    Update library.lua

--- library.lua
+++ library.lua
@@ -702,7 +702,7 @@ Rayfield.DisplayOrder = 100
 LoadingFrame.Version.Text = Release
 
 -- Thanks to Latte Softworks for the Lucide integration for Roblox
-local Icons = useStudio and require(script.Parent.icons) or loadWithTimeout('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/refs/heads/main/icons.lua')
+local Icons = useStudio and require(script.Parent.icons) or loadWithTimeout('https://raw.githubusercontent.com/unabletoIoad/vortexlib/main/icons.lua')
 -- Variables
 
 local CFileName = nil
